d5680b5fc7c2cbf721b5c95b8089ceacc5775d35
idon1nab <99960721+idon1nab@users.noreply.github.com>
2024-03-29 18:01:59 +0900

    feat: @PathVariable 추가

--- src/main/java/org/dallili/secretfriends/controller/DiaryController.java
+++ src/main/java/org/dallili/secretfriends/controller/DiaryController.java
@@ -66,7 +66,7 @@ public class DiaryController {
 
     @Operation(summary = "Delete Diary DELETE", description = "일기장 삭제/지인 매칭 취소")
     @DeleteMapping(value = "/{diaryID}")
-    public void diaryRemove(String diaryID) {
+    public void diaryRemove(@PathVariable("diaryID") String diaryID) {
 
         diaryService.removeDiary(diaryID);
 
