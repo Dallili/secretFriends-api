f896c1d5a064ba9edd0cd24c4ca4f628354e6ac6
idon1nab <99960721+idon1nab@users.noreply.github.com>
2024-04-02 17:57:42 +0900

    feat: 초대 코드 조회 메소드 작성

---
+++ src/main/java/org/dallili/secretfriends/controller/MatchingController.java
@@ -0,0 +1,33 @@
+package org.dallili.secretfriends.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.dallili.secretfriends.dto.DiaryDTO;
+import org.dallili.secretfriends.repository.DiaryRepository;
+import org.dallili.secretfriends.service.DiaryService;
+import org.dallili.secretfriends.service.MatchingHistoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/matches")
+public class MatchingController {
+
+    final MatchingHistoryService matchingHistoryService;
+
+    final DiaryService diaryService;
+
+
+    @Operation(summary = "Get Code GET", description = "초대 코드 리턴")
+    @GetMapping(value = "/{diaryID}")
+    public String codeDetails(String diaryID){
+
+        String code = diaryService.findOne(diaryID).getDiaryID();
+
+        return code;
+    }
+}
