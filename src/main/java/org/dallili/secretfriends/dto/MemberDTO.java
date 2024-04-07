96e4e28a6b811e9a711deee8a2f868ae83733991
Chaerin Hwang <chaa00@naver.com>
2024-04-07 12:00:26 +0900

    fix: 회원가입 요청 DTO 포맷 변경
    LocalDate 타입 필드 어노테이션 @NotBlank → @NotNull

--- src/main/java/org/dallili/secretfriends/dto/MemberDTO.java
+++ src/main/java/org/dallili/secretfriends/dto/MemberDTO.java
@@ -1,10 +1,7 @@
 package org.dallili.secretfriends.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
 
@@ -23,7 +20,7 @@ public class MemberDTO {
         @Size(max=20, message = "닉네임이 너무 깁니다. (최대 20글자)")
         private String nickname;
 
-        @NotBlank(message = "생년월일을 입력해주세요.")
+        @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd")
         private LocalDate birthday;
 
